11fb3a599b689968345045d25393d3729989738d
fengzi2016 <1820166258@qq.com>
2018-03-01 15:41:30 +0800

    feat:change card style

--- src/components/intro/intro_card.js
+++ src/components/intro/intro_card.js
@@ -12,7 +12,16 @@ export default class Card extends Component{
             <div className = "intro-position" style={{backgroundColor:props.info.position === '组长'?"rgb(253, 172, 31)": "rgb(170, 170, 170)" }}>{props.info.position}</div>
             <div className = "intro-name">姓名：{props.info.name}</div>
             <div className = "intro-tag">组别：{props.tag}</div>
+            <div className = 'intro-content'>
+            <div className='intro-words'>
+            <h2>座右铭：</h2>
+            {props.info.intro}
+            </div>
+            </div>
         </div>
+        
+        
+   
         </div>
         )
         
