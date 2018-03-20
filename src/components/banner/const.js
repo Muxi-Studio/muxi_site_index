d404f85eb58fb91a7f5d589992da52f741fba072
fengzi2016 <1820166258@qq.com>
2018-03-20 21:26:08 +0800

    feat:to web

--- src/components/banner/const.js
+++ src/components/banner/const.js
@@ -20,13 +20,14 @@ const conf = [{
 	img:'https://occc3ev3l.qnssl.com/index_site/product_muxiaoxi.png',
 	icon:"https://occc3ev3l.qnssl.com/index_site/icon_robot.png",
 },
-{
-	product:"木小犀",
-	type:"ios应用",
-	intro:"华师学子专属利器，具有查看课表",
-	href:"www.baidu.com",
-	img:'',
-	icon:"https://occc3ev3l.qnssl.com/index_site/icon_thinking.png",
-}]
+// {
+// 	product:"木小犀",
+// 	type:"ios应用",
+// 	intro:"华师学子专属利器，具有查看课表",
+// 	href:"www.baidu.com",
+// 	img:'',
+// 	icon:"https://occc3ev3l.qnssl.com/index_site/icon_thinking.png",
+// }
+]
 
 export default conf
