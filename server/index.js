8db534eba5d9e4fd3b44d17a14c5a0b145b41450
fengzi2016 <1820166258@qq.com>
2018-03-20 21:51:35 +0800

    feat:add to web

---
+++ server/index.js
@@ -0,0 +1,52 @@
+const send = require("koa-send");
+const Koa = require("koa");
+const Router = require("koa-router");
+const userAgent = require("koa-useragent");
+const path = require("path");
+const swig = require("swig");
+const router = new Router();
+const app = new Koa();
+
+const templateRoot = path.join(__dirname, "../dist/template");
+
+app.use(userAgent);
+
+router.get("/", function(ctx, next) {
+
+  let template = swig.compileFile(path.resolve(templateRoot, "second.html"));
+  ctx.body = template({});
+});
+
+router.get("/product", function(ctx, next) {
+  let template = swig.compileFile(path.resolve(templateRoot, "home.html"));
+  ctx.body = template({});
+});
+
+router.get("/intro", function(ctx, next) {
+  let template = swig.compileFile(path.resolve(templateRoot, "second.html"));
+  ctx.body = template({});
+});
+
+router.get("/member", function(ctx, next) {
+  let template = swig.compileFile(path.resolve(templateRoot, "intro.html"));
+  ctx.body = template({});
+});
+
+router.get("/join", function(ctx, next) {
+  let template = swig.compileFile(path.resolve(templateRoot, "join.html"));
+  ctx.body = template({});
+});
+
+
+
+router.get(/^\/static(?:\/|$)/, async ctx => {
+  let filepath = ctx.path.replace(/static\//, "");
+  await send(ctx, filepath, {
+    root: path.join(__dirname, "../dist")
+  });
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(3000);
+console.log("listening on port 3000");
